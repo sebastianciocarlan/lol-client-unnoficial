427c3c69adf0c168625b929afcc8e6ab2ca08fa4
sebastianciocarlan <sebastianciocarlan@gmail.com>
2024-04-24 11:15:57 +0200

    init download assets

--- src/app/init/init.component.ts
+++ src/app/init/init.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { LeagueService } from '../core/services/league/league.service';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { ElectronService } from '../core/services';
 
 @Component({
   selector: 'app-init',
@@ -11,20 +12,30 @@ import { Router } from '@angular/router';
   styleUrl: './init.component.scss'
 })
 export class InitComponent {
-  constructor(private riotService:LeagueService,private router:Router) {
+  constructor(private riotService:LeagueService,private router:Router,private electronService: ElectronService) {
 
   }
+  message = 'Hello World!';
   isLolStarted: boolean = false;
   isLolChecked: boolean = false;
   async ngOnInit(): Promise<void> {
+    this.message = "Descargando archivos para el cliente. Puede tomar tiempo."
+
+
+    await this.downloadNecessaryAssets();
+
+    this.message = "Checkeando si el lol esta encendido"
     try{
       await this.riotService.init({})
       this.isLolChecked = true;
       this.isLolStarted = true;
-      await this.router.navigate(['client']);
+      //await this.router.navigate(['client']);
     }catch(e){
       this.isLolChecked = true;
       console.log(e)
     }
+  }
+  async downloadNecessaryAssets(){
+
   }
 }
